4c7020f18dfc4efef52307cca876ac353b00b7f0
Calvin Hu <ycalvinhu@gmail.com>
2018-09-11 20:51:27 -0700

    Adjusted indentations

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,26 +1,26 @@
 // Check if browser supports Promises, if it doesn't add the polyfill for it
 if (!window.Promise) {
-    window.Promise = Promise
+  window.Promise = Promise
 }
 
 let deferredPrompt = null
 
 // Check if browser supports service workers
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then(() => {
-            console.log('Service worker registered!')
-        }).catch((error) => {
-            console.log(error)
-        })
+  navigator.serviceWorker.register('/sw.js')
+    .then(() => {
+      console.log('Service worker registered!')
+    }).catch((error) => {
+      console.log(error)
+    })
 }
 
 window.addEventListener('beforeinstallprompt', (event) => {
-    // Prevent chrome from showing the banner
-    console.log('beforeinstallprompt fired')
-    event.preventDefault()
-    deferredPrompt = event
-    return false
+  // Prevent chrome from showing the banner
+  console.log('beforeinstallprompt fired')
+  event.preventDefault()
+  deferredPrompt = event
+  return false
 })
 
 // This promise takes in a function (named or anonymous) which takes in two 
