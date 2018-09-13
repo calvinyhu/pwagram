604628897868e77b252756e980f0a4f230da6b16
Calvin Hu <ycalvinhu@gmail.com>
2018-09-12 20:41:38 -0700

    Added indexed db get

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,5 @@
+importScripts('/src/js/idb.js');
+
 const CACHE_STATIC_NAME = 'static-v1';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 const STATIC_FILES = [
@@ -6,6 +8,7 @@ const STATIC_FILES = [
   '/offline.html',
   '/src/js/app.js',
   '/src/js/feed.js',
+  '/src/js/idb.js',
   '/src/js/promise.js',
   '/src/js/fetch.js',
   '/src/js/material.min.js',
@@ -17,6 +20,11 @@ const STATIC_FILES = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 const URL = 'https://pwagramu.firebaseio.com/posts';
+
+const dbPromise = idb.open('posts-store', 1, db => {
+  if (!db.objectStoreNames.contains('posts'))
+    db.createObjectStore('posts', { keyPath: 'id' });
+});
 
 trimCache = (cacheName, maxItems) => {
   caches.open(cacheName).then(cache => {
@@ -75,11 +83,19 @@ self.addEventListener('fetch', event => {
 
 cacheThenNetwork = event => {
   event.respondWith(
-    caches.open(CACHE_DYNAMIC_NAME).then(cache => {
-      return fetch(event.request).then(response => {
-        cache.put(event.request, response.clone());
-        return response;
+    fetch(event.request).then(response => {
+      const clonedResponse = response.clone();
+      clonedResponse.json().then(data => {
+        for (let key in data) {
+          dbPromise.then(db => {
+            const tx = db.transaction('posts', 'readwrite');
+            const store = tx.objectStore('posts');
+            store.put(data[key]);
+            return tx.complete;
+          });
+        }
       });
+      return response;
     })
   );
 };
